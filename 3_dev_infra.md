eb00d5f7e3ddb8a8fb20496d46f04a3978f3e515
Alexander Mikheev <alexander.mikheev@saegus.com>
2023-12-04 17:14:55 +0100

    + power

--- 3_dev_infra.md
+++ 3_dev_infra.md
@@ -2,10 +2,12 @@ En 2023 un nouveau record de nombre de bornes de recharge disponibles au publiqu
 
 ![gr](img/3_cumul.svg)
 
-!!!! Chaque année de plus en plus de bornes sont mis en service. 
+Chaque année de plus en plus de bornes sont mis en service. L'année 2022 a été une année record jusqu'ici, sachant que 2023 n'est pas encore fini et que une partie de bornes mises en service n'ont pas encore été répértoriées. Ce qui explique le ralentissement visible en Q3 2023.
 
+Il est notable que la crise de COVID n'a pas pu ralentir le déploiement des installations. L'année 2020 a vu une hausse de 80% de nombre de bornes.
 
+On a la possibilité de voir aussi les jours quand un nombre important de bornes a été déployé en journée. Il s'agit par exemple de SAEMES qui a [ouvert](https://www.auto-infos.fr/article/saemes-et-totalenergies-inaugurent-un-parking-equipe-de-plus-de-500-bornes-de-recharge.248172) en mars 2020 500 bornes. Il peut également s'agir de dates approximatives liés à la publication de données sans que retrouve d'information dans la presse.
 
-!!!!Le Gouvernement ambitionne d'atteindre 400 000 points de recharge ouverts au public d'ici à 2030. Ainsi 
+Le Gouvernement ambitionne d'atteindre 400 000 points de recharge ouverts au public d'ici à 2030. Nous pouvons ésperer d'avoir encore plus de déploiements dans le futur. 
 
 
--- irve.py
+++ irve.py
@@ -2,115 +2,59 @@ import pandas as pd
 import numpy as np
 import sys
 import pathlib
-from datetime import datetime
 import plotly.express as px
 import plotly.io as pio
 from IPython.display import display, Markdown
 
 pio.templates.default = "plotly_dark"
 
+labels = {
+    "nom_amenageur": "Nom amenageur"
+    ,"nom_operateur": "Nom opérateur"
+    ,"nom_enseigne": "Nom enseigne"
+    ,"mis_en_service_cette_annee": "Mise en service en 2023"
+    ,"date_mise_en_service": "Date de mise en service"
+    ,"puissance_nominale_cat": "Puissance nominale"
+    ,"prise_type_combo_ccs": "Possède une prise combo"
+    ,"prise_type_2": "Possède une prise type 2"
+}
+
+
 df = pd.read_csv("data/consolidation-etalab-schema-irve-statique-v-2.2.0-20231130.csv" 
                 ,low_memory=False
-                ,parse_dates=["date_mise_en_service"])\
+                ,parse_dates=["date_mise_en_service"]
+                ,dtype={"consolidated_code_postal": str
+                       })\
   .sort_values(['id_pdc_itinerance', 'last_modified'])\
 
+df_filter = df["id_pdc_itinerance"]=="Non concerné"
+
 df = pd.concat([
-  df[df['id_pdc_itinerance']!="Non concerné"].drop_duplicates('id_pdc_itinerance', keep='last'),
-  df[df['id_pdc_itinerance']=="Non concerné"]
+  df[~df_filter].drop_duplicates('id_pdc_itinerance', keep='last'),
+  df[df_filter]
 ])
 
+# Date de mise en service
 df.loc[df["date_mise_en_service"]<pd.to_datetime("2010-01-01"),"date_mise_en_service"]=None
-
-def Get_final_table():
-    
-    table_schema = pd.read_csv("Data/IRVE_itinerance_valide2023-05-24.csv").drop(columns=["Unnamed: 0","index"])
-    table_schema.siren_amenageur = table_schema.siren_amenageur.astype(str)
-    table_schema['code_departement'] = table_schema.consolidated_code_postal.apply(lambda x : str(x)[:2])
-    
-    
-    table_depart = pd.read_csv("Data/departements-france.csv")
+df["mis_en_service_cette_annee"] = np.where(df["date_mise_en_service"].dt.year==pd.to_datetime("now").year 
+         , "Oui"
+         , "Non")
 
 
-    res = pd.merge(table_schema,table_depart, how='inner', on='code_departement')
-   
-    return res 
-    
+# Département
+df_dep = pd.read_csv("Data/departements-france.csv"
+                     ,dtype={"code_departement": str})
+df["code_departement"] = df["consolidated_code_postal"].str[:2]
 
-def Show_repartition(table,group,by,first_elements):
-    
-    if not isinstance(table[group].tolist()[0],str):
-        table[group] = table[group].astype(str)
-    df = table.groupby(group)[by].count().reset_index(name="nombre total").sort_values(by="nombre total", ascending=False).head(first_elements)
-    fig = px.bar(df, x=group, y="nombre total")
-    return fig 
+df = pd.merge(df,df_dep, how='left', on='code_departement')
 
+# Power
+df["puissance_nominale_cat"] = pd.cut(\
+  df["puissance_nominale"]\
+  .apply(lambda x: x/1000 if x >1000 else x)\
+,[0,1.8,3.5,7.5,26,52,151,500]
+,labels=["1.7","3.4","7.5","22","50","150",">150"]
+, include_lowest=False)
 
-def Show_pdc_per_department(df):
-    
-    app = dash.Dash(__name__)
-
-    app.layout = html.Div([
-        html.H1("Visualisation des Points de Charge par Département en France"),
-
-        # RadioButton pour choisir entre "aménageurs" et "enseigne"
-        dcc.RadioItems(
-            id='choice-radio',
-            options=[
-                {'label': 'Aménageurs', 'value': 'aménageurs'},
-                {'label': 'Enseigne', 'value': 'enseigne'}
-            ],
-            value='aménageurs',
-            labelStyle={'display': 'block'}
-        ),
-
-        # Dropdown qui change en fonction du choix du RadioButton
-        dcc.Dropdown(id='dynamic-dropdown'),
-
-        dcc.Graph(id='carte-pdc')
-    ])
-
-    @app.callback(
-        Output('dynamic-dropdown', 'options'),
-        Output('dynamic-dropdown', 'value'),
-        Input('choice-radio', 'value')
-    )
-    def update_dropdown(choice):
-        if choice == 'aménageurs':
-            options = [{'label': amenageur, 'value': amenageur} for amenageur in df['nom_amenageur'].dropna().unique()]
-            return options, options[0]['value']
-        elif choice == 'enseigne':
-            options = [{'label': enseigne, 'value': enseigne} for enseigne in df['nom_enseigne'].dropna().unique()]
-            return options, options[0]['value']
-
-    @app.callback(
-        Output('carte-pdc', 'figure'),
-        [Input('dynamic-dropdown', 'value'),
-         Input('choice-radio', 'value'),
-         Input('carte-pdc', 'relayoutData')]
-    )
-    def update_map(selected_value, choice, relayoutData):
-        if choice == 'aménageurs':
-            filtered_df = df[df['nom_amenageur'] == selected_value]
-        elif choice == 'enseigne':
-            filtered_df = df[df['nom_enseigne'] == selected_value]
-
-        fig = px.choropleth_mapbox(filtered_df,
-                                   geojson='https://france-geojson.gregoiredavid.fr/repo/departements.geojson',
-                                   locations='code_departement',
-                                   featureidkey="properties.code",
-                                   color='nbre_pdc',
-                                   color_continuous_scale="Viridis",
-                                   mapbox_style="carto-positron",  
-                                   zoom=5, center = {"lat": 46.603354, "lon": 1.888334},
-                                   opacity=0.5,
-                                   labels={'nbre_pdc':'Nombre de Points de Charge'})
-
-        fig.update_layout(title=f'Points de Charge par Département pour {selected_value}',
-                          margin={"r":0,"t":40,"l":0,"b":0},
-                          mapbox=dict(bearing=0, pitch=0)
-                         )
-        return fig
-
-    app.run_server(debug=True)
-
-
+df["prise_type_combo_ccs"] = df["prise_type_combo_ccs"].str.lower().map({"0":"Non","false":"Non","1":"Oui","true":"Oui"})
+df["prise_type_2"] = df["prise_type_2"].str.lower().map({"0":"Non","false":"Non","1":"Oui","true":"Oui"})
