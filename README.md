0de122315e129f9911ae83234d910669f764e225
Alexander Mikheev <alexander.mikheev@saegus.com>
2023-12-04 11:34:29 +0100

    draft 1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # IRVE
 
 The goal of this project is to analyse open data of infrastructure of electric vehciles and identify trends and insights
+
+# Build
+1. Download as md
+2. pandoc rapport.md -s -o irve.docx
--- irve.py
+++ irve.py
@@ -1,9 +1,4 @@
 import pandas as pd
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-from jupyter_dash import JupyterDash
 import numpy as np
 import sys
 import pathlib
